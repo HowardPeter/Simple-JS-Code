ef5df03dc048c9ffa0ad93cee194e4e49d8de160
HowardPeter <phuocnt1611@gmail.com>
2024-04-22 19:22:50 +0700

    incompleted

--- index.js
+++ index.js
@@ -48,54 +48,6 @@ function outputComponents(computer_components) {
     return htmlTable;
 }
 
-function deleteOutputs(computer_components) {
-    let htmlTable = ``
-    htmlTable += `<style>
-                        table {
-                            border-collapse: collapse;
-                            width: 100%;
-                        }
-                        th, td {
-                            border: 1.2px solid #dddddd;
-                            padding: 8px;
-                        }
-                        th {
-                            background-color: #bebebe;
-                            text-align: center;
-                        }
-                        button{
-                            border: 0;
-                            background-color: transparent;
-                            cursor: pointer;
-                            color: #004080;
-                            font-size: 15px;
-                        }
-                        button:hover {
-                            color: blueviolet;
-                        }
-                    </style>`
-    htmlTable += '<table border="1">';
-    htmlTable += '<tr><th>ID</th><th>Name</th><th>Supplier</th><th>Price</th><th>Units on Stock</th><th>Image</th><th>Action</th></tr>';
-    computer_components.forEach(component => {
-        htmlTable += `<tr><td style="text-align: center;">${component.id}</td>
-                        <td>${component.name}</td>
-                        <td>${component.supplier}</td>
-                        <td style="text-align: center;">${component.price}$</td>
-                        <td style="text-align: center;">${component.unitonstock}</td>
-                        <td style="text-align: center;"><img src="${component.image}" alt="" width = 80px></td>
-                        <td style="text-align: center;"><a href="/public/pages/deleteHandler.html"></td>
-                    </tr>`;
-    });
-    htmlTable += '</table>'
-    htmlTable += '<button style="margin-top: 20px; margin-bottom: 10px; margin-left: 94%;">Delete All</button>'
-    return htmlTable;
-}
-async function deleteComp(id) {
-    await client.connect();
-    compsCollection = client.db(myDB).collection(collectionComp);
-
-    await compsCollection.deleteOne(id);
-}
 async function getNewID(client, dbName, collectionName) {
     const db = client.db(dbName);
     const collection = db.collection(collectionName);
@@ -271,12 +223,12 @@ app.get('/readDelete', async (req, res) => {
             query.supplier = { $regex: delete_sup, $options: 'i' };
         }
 
-        const result = await compsCollection.find(query).sort({ id: -1 }).toArray();
+        const result = await compsCollection.find(query).toArray();
 
         if (!result) {
             res.send("Component not found!");
         } else {
-            const queryParams = queryString.stringify(result);
+            const queryParams = new URLSearchParams({ results: JSON.stringify(result) }).toString();
             res.redirect(`/deleteHandler.html?${queryParams}`);
         }
     } catch (error) {
@@ -286,5 +238,22 @@ app.get('/readDelete', async (req, res) => {
         await client.close();
     }
 });
+
+app.delete('/delete/:id', async (req, res) => {
+    
+    try {
+        await client.connect();
+        compsCollection = client.db(myDB).collection(collectionComp);
+        
+        const id = req.params.id;
+
+        await compsCollection.deleteOne({id: id});
+    } catch (error) {
+        res.send(error)
+    }
+    finally {
+        await client.close();
+    }
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
